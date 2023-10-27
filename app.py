81f5ae3dafb9849b83df197cc0e9f97f2e6101be
Michael David <lordkay07@gmail.com>
2023-10-27 10:24:04 +0100

    add the first and last commit for this short program

---
+++ app.py
@@ -0,0 +1,16 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+
+print("Your account has een created successfully!")
+print("Login Now")
+
+
+username2 = input("Enter username: ")
+password2 = input("Enter password: ")
+
+
+if username == username2 and password == password2:
+    print("Login Successful")
+else:
+    print("Invalid credentials")
